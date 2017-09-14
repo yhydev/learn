adda04759006fc74eb9c8f951a291bdf69ecb819
met0 <812783704@qq.com>
2017-09-14 17:52:42 +0800

    <no finish>  位于分支 tmp  要提交的变更： 	修改:         request/app.py

--- flask/request/app.py
+++ flask/request/app.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python
 
-from flask import Flask,request
+from flask import Flask,request,abort,session
 
 app = Flask(__name__)
+
+
+#response status setting
+@app.route("/abort/<int:status>")
+def abort_request(status):
+	abort(status)
+	return status;
+
+
+#session set
+@app.route("/session/<name>/<value>", methods = ["POST"])
+def session_post(name,value):
+	session["name"] = value
+	return "add success"
+
+#session get
+@app.route("/session/<name>", methods = ["GET"])
+def session_get(name):
+	return session[name]
+	
+#session delete
+@app.route("/session/<name>", methods = ["DELETE"])
+def session_delete():
+	session.pop(name, None)
+	return "delete success"
+
+
 
 @app.route("/<name>")
 def req():
